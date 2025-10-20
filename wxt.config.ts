f1843ca1b33376a3905948153e97ca536363449d
Aviad Hadad <aviad@sthuck.info>
2025-10-20 11:01:53 +0300

    fix permissions

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'wxt';
+import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  srcDir: 'src',
-  modules: ['@wxt-dev/module-svelte'],
+  srcDir: "src",
+  modules: ["@wxt-dev/module-svelte"],
+  manifest: {
+    permissions: ["scripting"],
+    host_permissions: ["https://mail.google.com/*"],
+  },
 });
